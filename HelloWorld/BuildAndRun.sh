fa63cdc7403cc44bd4f7685f1363f86e9c89854b
Kevin Boss <kevin.boss@outlook.com>
2018-12-26 20:00:42 +0100

    -

---
+++ HelloWorld/BuildAndRun.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+#/bin/bash
+
+dotnet restore
+dotnet build --no-restore
+
+# Run the 2 console apps in different windows
+
+dotnet run --project ./src/SiloHost --no-build & 
+sleep 10
+dotnet run --project ./src/OrleansClient --no-build &
